8f80f20d57bd3904a8ffe61ec11386f3ee1280b6
Johannes Donath <johannesd@torchmind.com>
2016-06-22 12:05:29 +0200

    Added a profile property representation.

---
+++ src/main/java/net/minepay/mcapi/mojang/ProfileProperty.java
@@ -0,0 +1,32 @@
+package net.minepay.mcapi.mojang;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Provides a representation for profile properties.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class ProfileProperty<T> {
+    private final String name;
+    private final T value;
+
+    @JsonCreator
+    public ProfileProperty(@Nonnull @JsonProperty("name") String name, @Nonnull @JsonProperty("value") T value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Nonnull
+    public String getName() {
+        return this.name;
+    }
+
+    @Nonnull
+    public T getValue() {
+        return this.value;
+    }
+}
