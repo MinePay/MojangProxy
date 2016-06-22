31580a88e00891c05e84434617e3b71ceb799bb2
Johannes Donath <johannesd@torchmind.com>
2016-06-22 12:09:45 +0200

    Added thread safety information.

--- src/main/java/net/minepay/mcapi/mojang/ProfileProperty.java
+++ src/main/java/net/minepay/mcapi/mojang/ProfileProperty.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Provides a representation for profile properties.
  *
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
+@Immutable
+@ThreadSafe
 public class ProfileProperty<T> {
     private final String name;
     private final T value;
