0412b0934bebea488c223998273c7d7194fad8b3
Johannes Donath <johannesd@torchmind.com>
2016-06-23 22:54:07 +0200

    Added an endpoint in order to look up multiple names at once

--- src/main/java/net/minepay/mcapi/controller/v1/NameController.java
+++ src/main/java/net/minepay/mcapi/controller/v1/NameController.java
@@ -7,6 +7,7 @@ import net.minepay.mcapi.repository.MojangCache;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -42,6 +45,8 @@ public class NameController {
      * @param name      a display name.
      * @param timestamp a timestamp.
      * @return a profile name.
+     *
+     * @throws IOException when requesting data from Mojang fails.
      */
     @Nonnull
     @RequestMapping(path = "/{name}", method = RequestMethod.GET)
@@ -76,5 +81,46 @@ public class NameController {
         }
 
         return identifier;
+    }
+
+    /**
+     * Attempts to find a list of UUIDs which have been associated with a specified display name.
+     *
+     * @param names a list of display names.
+     * @return a list of profile name.
+     *
+     * @throws IOException when requesting data from Mojang fails.
+     */
+    @Nonnull
+    @RequestMapping(path = "/{name}", method = RequestMethod.POST)
+    public List<ProfileName> lookupIdentifiers(@Nonnull @RequestBody List<String> names) throws IOException {
+        if (names.size() > 100) {
+            throw new IllegalArgumentException("Cannot poll more than 100 names at once");
+        }
+
+        // check cache first
+        List<ProfileName> identifiers = new ArrayList<>();
+
+        for (String name : names) {
+            ProfileName identifier = this.cache.findIdentifier(name);
+
+            if (identifier != null) {
+                identifiers.add(identifier);
+                names.remove(name);
+            }
+        }
+
+        // poll from Mojang if any data is left to process
+        if (names.size() > 0) {
+            List<ProfileName> tmp = this.client.findIdentifier(names);
+
+            if (tmp != null) {
+                // TODO: Pipeline?
+                tmp.forEach(this.cache::saveIdentifier);
+                identifiers.addAll(tmp);
+            }
+        }
+
+        return identifiers;
     }
 }
